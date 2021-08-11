8932821d947ac4cbb0866bf5a321b128e57c2146
StakeHub <83736116+StakeHubFinance@users.noreply.github.com>
2021-08-11 06:19:25 -0400

    Update BulkSender.sol

--- BulkSender.sol
+++ BulkSender.sol
@@ -303,23 +303,121 @@ library SafeMath {
     }
 }
 
-contract BulkSender {
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+contract BulkSender is Ownable {
     using SafeMath for uint256;
     
-    uint256 public txFee = 0.01 ether;
-    uint256 public memberFee = 0.5 ether;
+    uint256 public txFee;
+    uint256 public memberFee;
     
     address payable feeWallet;
     
     mapping(address => bool) memberStatus;
     
-    function setFeeWallet(address payable _wallet) public payable {
-        feeWallet = _wallet;
-    }
-    
     receive() external payable {}
 
     fallback() external payable {}
+    
+    function setFeeWallet(address payable _wallet) public payable onlyOwner {
+        feeWallet = _wallet;
+    }
+    
+    function setTxFee(uint256 _amount) public onlyOwner{
+        txFee = _amount;
+    }
+    
+    function setMemberFee(uint256 _amount) public onlyOwner {
+        memberFee = _amount;
+    }
+    
+    function revokeMember(address _toBeRevoked) public onlyOwner {
+        memberStatus[_toBeRevoked] = false;
+    }
     
     function buyMembership() external payable {
         require(msg.value >= memberFee);
@@ -327,11 +425,20 @@ contract BulkSender {
         memberStatus[msg.sender] = true;
     }
     
-    function bulksendToken(IERC20 _token, address[] calldata _to, uint256[] calldata _values) public  
+    function isMember(address _member) public view returns(bool) {
+        return memberStatus[_member];
+    }
+    
+    function bulksendToken(IERC20 _token, address[] calldata _receivers, uint256[] calldata _values) public payable 
     {
-        require(_to.length == _values.length);
-        for (uint256 i = 0; i < _to.length; i++) {
-        require(_token.transferFrom(msg.sender, _to[i], _values[i]));
+        uint256 msgValue = msg.value;
+        bool memStatus = isMember(msg.sender);
+        if(memStatus == false) {
+            require(msgValue >= txFee);
+        }
+        require(_receivers.length == _values.length);
+        for (uint256 i = 0; i < _receivers.length; i++) {
+        require(_token.transferFrom(msg.sender, _receivers[i], _values[i]));
         }
     }
     
